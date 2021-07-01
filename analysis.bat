d5efdecf577eaf03ad4cf27db3d4c55d10606b41
Nadia <nminingo@gmu.edu>
2021-07-01 10:39:19 -0400

    remove files that was not supposed to be tracked because .gitignore was not yet updated

---
+++ analysis.bat
@@ -0,0 +1,1 @@
+python3 /home/nadia/NeuroRDanal/NeuroRDanal/nrdh5_anal.py Model_Cof [inter SSHPAKold] [ppERK]
---
+++ cof.bat
@@ -0,0 +1,13 @@
+
+#java -jar /home/nadia/neurord-3.2.4-all-deps.jar -t 900000 Model_Cof-SSH1500-PDE4General.xml&
+
+#java -jar /home/nadia/neurord-3.2.4-all-deps.jar -t 900000 Model_Cof-SSH1500-PDE4GeneralBASAL.xml
+
+
+#java -jar /home/nadia/neurord-3.2.4-all-deps.jar -t 900000 Model_Cof-SSH2000-PDE4General.xml
+
+#java -jar /home/nadia/neurord-3.2.4-all-deps.jar -t 900000 Model_Cof-SSH1500-PDE4General-stim.xml Model_Cof-SSH1500-PDE4General-TstimCaL&
+
+java -jar /home/nadia/neurord-3.2.4-all-deps.jar -t 900000 Model_Cof-SSH1500-PDE4General-stimRacPAK.xml Model_Cof-SSH1500-PDE4General-TstimRacPAK&
+
+#java -jar /home/nadia/neurord-3.2.4-all-deps.jar -t 900000 Model_Cof-SSH2000-PDE4General-stim.xml
